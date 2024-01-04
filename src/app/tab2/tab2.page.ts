34e15c4e3597309ef0588a57d594f21b3bdfda58
daniellelobas <daniellelobas@gmail.com>
2024-01-04 20:06:00 -0300

    atualizacao

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+  
+  oficinaAutomotiva(){
+    this.abrirUrl('https://www.google.com/maps/search/Oficinas+automotiva/@-3.8185156,-38.6274703,13.25z?entry=ttu')
+  }
 
+  autoEletrica(){
+  this.abrirUrl('https://www.google.com/maps/search/Oficinas+autoeletrica/@-3.8665023,-38.6988725,11.25z?entry=ttu')
+  
+  }
+  autoCenter(){
+    this.abrirUrl('https://www.google.com/maps/search/Oficinas+autoCenter/@-3.8496973,-38.5876897,13z/data=!3m1!4b1?entry=ttu')
+  }
+
+
+  
+
+  abrirUrl(url: string){
+    window.open(url, `blank`)?.focus();
+  } 
 }
+
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page  implements OnInit {
 
-  constructor() {}
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    // private contatoService: ContatoService,
+    public toastController: ToastController
+    ) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      endereco: ['', [Validators.required]],
+    });
+  }
+  
+  Whats() {
+    this.abrirUrl('https://api.whatsapp.com/send?phone=5585991127391&text=Como%20posso%20te%20ajudar?')
+  }
+
+  abrirUrl(url: string){
+    window.open(url, `blank`)?.focus();
+  } 
+  enviar() {
+    console.log('enviei')
+  }
 
 }
